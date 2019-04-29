816287ff1084c2039594a27624ff582dd580fb82
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-04-29 14:34:41 +0400

    Expose redux api

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import App from './components/App';
 
 import store from './redux/store';
 
+import * as actions from './redux/actions';
+const tests = localStorage.getItem('isTestRunning');
+if (tests) {
+  // window._store.dispatch(window._actions.addTask({ title: 'xxxx', priority: 1}))
+  window._store = store;
+  window._actions = actions;
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
